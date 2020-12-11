7e0b43157698606639e2c2c5ed7645f899146d84
Ted Delin <ted@delin.tech>
2020-12-11 11:20:22 +0100

    Sum the intersection

--- aoc_six/form_filler.py
+++ aoc_six/form_filler.py
@@ -8,6 +8,12 @@ class Group:
     def get_value(self):
         return len(set(self.submission.replace(" ", "")))
 
+    def get_intersect_value(self):
+        individual_answers = [set(x) for x in self.submission.split(" ")]
+        del individual_answers[0]
+        u = individual_answers[0].intersection(*individual_answers)
+        return len(u)
+
 
 class FormCalculator:
     def __init__(self, lines):
@@ -16,6 +22,9 @@ class FormCalculator:
     def calculate(self):
         return sum([group.get_value() for group in self.groups])
 
+    def calculate_intersect(self):
+        return sum([group.get_intersect_value() for group in self.groups])
+
 
 if __name__ == "__main__":
     file_handler = FileHandler("input.txt")
@@ -23,3 +32,4 @@ if __name__ == "__main__":
     file_handler.join_on_not_empty()
     form_calculator = FormCalculator(file_handler.file_lines)
     print(form_calculator.calculate())
+    print(form_calculator.calculate_intersect())
